8c29508712053fcb18ca2b6d9710619cd74d5520
Aksa-Carvalho <aksacarvalho43416@gmail.com>
2026-04-23 16:13:17 -0300

    criando o primeiro componente

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
+import Button from "../components/Button";
+
 const Home = () => {
   return (
     <div>
-      <h2 >Home</h2>
+      <h2>Home</h2>
+      <Button variant="outline">Clique em mim</Button>
+      
     </div>
   );
 };
+
 
 export default Home;
