6fe26cf3fd4cae2ccb4425637d9517f1b4416651
clemilsonchaves <153899374+aksacarvalho@users.noreply.github.com>
2026-05-05 16:10:08 -0300

    Login com Google + Firebase

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,13 @@
 import GoogleLoginButton from "../components/GoogleLoginButton";
+import { signInWithPopup } from "firebase/auth";
 
+import { firebaseAuth, googleAuthProvider } from "../components/config/firebase";
 const Login = () => {
-   
-  function handleLogin(): void {
-    throw new Error("Function not implemented.");
-  }
+   const handleLogin = async () => {
+     const result = await signInWithPopup(firebaseAuth, googleAuthProvider);
+      console.log(result);
+   };
+
 
    return (
    <div  className="min-h-screen flex items-center justify-center bg-gray-200 py-12 px-4 sm:px-6 lg:px-8 ">
